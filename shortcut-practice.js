bf28cfb0fd23244c888a10bbccf4dd319df2228d
Mahim Arib <mahim216@gmail.com>
2018-07-10 15:49:17 -0400

    finished excercise

---
+++ shortcut-practice.js
@@ -0,0 +1,27 @@
+// 1. Decrease the variable below by 1 using two different shortcuts.
+// Print out its value after each step to make sure it worked.
+let a = 7;
+a += 1;
+console.log(a); // 8
+a++;
+console.log(a); // 9
+
+// 2. Double the value of the variable below using a multiplication shortcut.
+// Print out its value before and after each step to make sure it worked.
+
+let b = 18;
+console.log(b); // 18
+b *= 2;
+console.log(b); // 36
+
+// 3. Predict the value of d. Then print it out to see if you were right.
+let c = 13;
+let d = c++;
+console.log('expected: 13', '\n', 'actual: ' + d);
+
+// 4. Move the decimal two spots to the left using a shortcut.
+// Print out the value of e before and after to make sure it worked.
+let e = 234.5;
+console.log(e);
+e *= (1E-2);
+console.log(e);
