16594b3854587ff8384e682c993bbfb9c5f94fd4
Mahim Arib <mahim216@gmail.com>
2018-07-10 15:34:01 -0400

    completed bonus

--- pythagorean-challenge.js
+++ pythagorean-challenge.js
@@ -1,2 +1,15 @@
 // Author: Mahim Arib
-console.log("give me all three sides of a right triangle and I can tell if it is a triple");
+
+console.log('give me all three sides of a right triangle and I can tell if it is a triple, in positive integer value');
+
+const readline = require('readline-sync');
+
+const smallLeg = parseInt(readline.question('small leg: '));
+const bigLeg = parseInt(readline.question('big leg: '));
+const hypothenuse = parseInt(readline.question('hypothenuse: '));
+
+if (hypothenuse ** 2 == smallLeg ** 2 + bigLeg ** 2) {
+  console.log('This is a pythagorean triple');
+} else {
+  console.log('This is not a pythagorean triple');
+}
